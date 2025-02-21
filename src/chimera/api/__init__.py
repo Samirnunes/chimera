9ea574a787b3108b4a935384cccdc095db3417bd
Samir Nunes <samir.silva12342@gmail.com>
2025-02-20 22:52:39 -0300

    fix: change filename

--- src/chimera/api/__init__.py
+++ src/chimera/api/__init__.py
@@ -5,9 +5,9 @@ from .container import TRAIN_LABELS_FILENAME as TRAIN_LABELS_FILENAME
 from .dto import FitOutput as FitOutput
 from .dto import PredictInput as PredictInput
 from .dto import PredictOutput as PredictOutput
-from .paths import CHIMERA_ENSEMBLE_FIT_PATH as CHIMERA_ENSEMBLE_FIT_PATH
-from .paths import CHIMERA_ENSEMBLE_PREDICT_PATH as CHIMERA_ENSEMBLE_PREDICT_PATH
-from .paths import CHIMERA_NODE_FIT_PATH as CHIMERA_NODE_FIT_PATH
-from .paths import CHIMERA_NODE_PREDICT_PATH as CHIMERA_NODE_PREDICT_PATH
+from .path import CHIMERA_ENSEMBLE_FIT_PATH as CHIMERA_ENSEMBLE_FIT_PATH
+from .path import CHIMERA_ENSEMBLE_PREDICT_PATH as CHIMERA_ENSEMBLE_PREDICT_PATH
+from .path import CHIMERA_NODE_FIT_PATH as CHIMERA_NODE_FIT_PATH
+from .path import CHIMERA_NODE_PREDICT_PATH as CHIMERA_NODE_PREDICT_PATH
 from .response import build_error_response as build_error_response
 from .response import build_json_response as build_json_response
