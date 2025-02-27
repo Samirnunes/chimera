15daa8e515979417e32ab296b00868da43555faf
Samir Nunes <samir.silva12342@gmail.com>
2025-02-26 21:36:49 -0300

    feat: add dns to each worker

--- src/chimera/containers/handler.py
+++ src/chimera/containers/handler.py
@@ -22,6 +22,7 @@ class ContainersHandler:
         for i in range(len(self._workers_config.CHIMERA_WORKERS_NODES_NAMES)):
             self._build_docker_image(i)
             self._run_container(i)
+            self._add_dns_entries_to_container(i)
 
     def _sanity_checks(self) -> None:
         if len(self._workers_config.CHIMERA_WORKERS_NODES_NAMES) != len(
@@ -128,6 +129,32 @@ class ContainersHandler:
             container_ip,
             "--cpu-shares",
             str(cpu_shares),
+            "--add-host",
+            self._workers_config.CHIMERA_WORKERS_NODES_NAMES[i] + ":" + container_ip,
             image_name,
         ]
         subprocess.run(cmd, check=True)
+
+    def _add_dns_entries_to_container(self, i: int) -> None:
+        """Adds DNS entries for all workers to each worker's /etc/hosts file."""
+        container_name = self._workers_config.CHIMERA_WORKERS_NODES_NAMES[i]
+        for j in range(len(self._workers_config.CHIMERA_WORKERS_NODES_NAMES)):
+            if i == j:
+                continue
+            other_container_ip = (
+                f"{self._network_config.CHIMERA_NETWORK_PREFIX}.{j + 2}"
+            )
+            other_dns_name = self._workers_config.CHIMERA_WORKERS_NODES_NAMES[j]
+
+            cmd = [
+                "docker",
+                "exec",
+                container_name,
+                "sh",
+                "-c",
+                f"echo '{other_container_ip} {other_dns_name}' >> /etc/hosts",
+            ]
+            subprocess.run(cmd, check=True)
+            print(
+                f"Added DNS entry '{other_dns_name}:{other_container_ip}' to container '{container_name}'"
+            )
