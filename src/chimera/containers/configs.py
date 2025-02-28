2300bd077839ec38cbd31cbc7c6cf7489a48e377
Samir Nunes <samir.silva12342@gmail.com>
2025-02-27 22:09:08 -0300

    feat: add test constants

--- src/chimera/containers/configs.py
+++ src/chimera/containers/configs.py
@@ -11,6 +11,8 @@ CHIMERA_WORKERS_FOLDER = "chimera_workers"
 CHIMERA_TRAIN_DATA_FOLDER = "chimera_train_data"
 CHIMERA_TRAIN_FEATURES_FILENAME = "X_train.csv"
 CHIMERA_TRAIN_LABELS_FILENAME = "y_train.csv"
+CHIMERA_TEST_FEATURES_FILENAME = "X_test.csv"
+CHIMERA_TEST_LABELS_FILENAME = "y_test.csv"
 
 
 class NetworkConfig(BaseSettings):
