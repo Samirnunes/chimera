7934453ad58f3eb34158b61509f67dd6febb0577
Samir Nunes <samir.silva12342@gmail.com>
2025-03-14 22:48:03 -0300

    fix: examples

--- examples/scripts/chimera_rand_forest/chimera_workers/tree2.py
+++ examples/scripts/chimera_rand_forest/chimera_workers/tree2.py
@@ -2,9 +2,9 @@ from sklearn.tree import DecisionTreeRegressor
 
 from chimera.nodes.workers import RegressionWorker
 
-node = RegressionWorker(
+worker = RegressionWorker(
     DecisionTreeRegressor(max_depth=2, max_leaf_nodes=5), bootstrap=True
 )
 
 if __name__ == "__main__":
-    node.serve()
+    worker.serve()
