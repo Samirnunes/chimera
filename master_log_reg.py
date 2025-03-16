81d7c05ececa3598ff98326be67c92ce86336d1f
Samir Nunes <samir.silva12342@gmail.com>
2025-03-16 16:50:25 -0300

    fix: add more workers for sgd

--- master_log_reg.py
+++ master_log_reg.py
@@ -4,7 +4,7 @@ import chimera
 from chimera.nodes import ParameterServerMaster
 
 os.environ["CHIMERA_WORKERS_NODES_NAMES"] = (
-    '["sgd1", "sgd2", "sgd3", "sgd4", "sgd5", "sgd6", sgd7"]'
+    '["sgd1", "sgd2", "sgd3", "sgd4", "sgd5", "sgd6", "sgd7"]'
 )
 os.environ["CHIMERA_WORKERS_CPU_SHARES"] = "[2, 2, 2, 2, 2, 2, 2]"
 os.environ["CHIMERA_WORKERS_MAPPED_PORTS"] = "[81, 82, 83, 84, 85, 86, 87]"
