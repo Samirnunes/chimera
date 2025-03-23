401b69b1e61884344ce40cf352e99c13f41c957a
Samir Nunes <samir.silva12342@gmail.com>
2025-03-23 15:57:59 -0300

    fix: __init__.py

--- src/chimera/utils/__init__.py
+++ src/chimera/utils/__init__.py
@@ -1,3 +1,3 @@
-from .logger import parse_times_file as parse_times_file
 from .logger import status_logger as status_logger
 from .logger import time_logger as time_logger
+from .parse import parse_times_file as parse_times_file
